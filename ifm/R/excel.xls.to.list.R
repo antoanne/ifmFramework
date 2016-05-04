f1b87606b6b4b14ad0b31883fd9295dc71e15ce2
afcosta-ibm <afcosta@br.ibm.com>
2016-05-04 18:40:14 -0300

    create excel function

---
+++ ifm/R/excel.xls.to.list.R
@@ -0,0 +1,69 @@
+#' Extract a list of variables from the spreadsheet to be used on the maxNPV
+#'  function.
+#' 
+#' @name excel.xls.to.list
+#' 
+#' @aliases excel_xls_to_list excelXlsToList
+#' 
+#' @param xls.spreadsheet.path The complete path to the spreadsheet that
+#' represents the project.
+#' 
+#' @return List of variables to be used on the maxNPV function.
+#' 
+#' @export
+#' 
+#' @description This function is responsible for reading a spreadsheet 
+#' representing the project, and return a list with the following information
+#' (in this order): The interest rate, the list of activities, the list of 
+#' durations of activities, the list of predecessors of activities and the 
+#' matrix that represents the cash flow series
+#' 
+#' @keywords excel maxNPV xls.to.list
+#' 
+#' @family utility
+#' 
+#' @examples
+#'
+#' list.of.variables <- excel.xls.to.list("/home/user/project/spreadsheet.xls")
+#' 
+#' list.of.variables <- excel.xls.to.list("C://user//project//spreadsheet.xls")
+#' 
+
+require("XLConnect")
+
+excel.xls.to.list <- excel_xls_to_list <- excelXlsToList <-
+  function(xls.spreadsheet.path){
+    
+    # load the workbook
+    workbook <- loadWorkbook(xls.spreadsheet.path, create = FALSE)
+    
+    # data frame that represents the cash flow series matrix
+    data.frame.cash.flow.series <- readNamedRegion(workbook, name = "cfs", 
+                                                   header = FALSE)
+    
+    # data frame that represents the vector of predecessors
+    data.frame.predecessors <- readNamedRegion(workbook, name = "predecessor", 
+                                               header = FALSE)
+    
+    # data frame that represents the vector of durations
+    data.frame.durations <- readNamedRegion(workbook, name = "duration", 
+                                            header = FALSE)
+    
+    # data frame that represents the vector of activities
+    data.frame.activities <- readNamedRegion(workbook, name = "activities", 
+                                             header = FALSE)
+    
+    # data frame that represents the interest rate
+    data.frame.interest.rate <- readNamedRegion(workbook, 
+                                                name = "interestrate",
+                                                header = FALSE)
+
+    # list of variables to be used in maxNPV
+    variables.list <- list(data.frame.interest.rate[[1]], 
+                           data.frame.activities[[1]], 
+                           data.frame.durations[[1]],
+                           data.frame.predecessors[[1]], 
+                           data.frame.cash.flow.series)
+    
+    return (variables.list)
+  }
---
+++ ifm/R/npvg.r
@@ -0,0 +1,12 @@
+npvg<-function (csf,r=0.01) {
+#returns the net present value discount vector of a generalized cash flow stream
+#input: relation csf={(vi,ti)} where vi is the element at time ti (byrow=F)
+#ti is an integer
+d<-vector(length=n)
+d<-1/(1+r)^csf[,2]
+v<-sum(d*csf[,1])
+v
+}
+
+
+
