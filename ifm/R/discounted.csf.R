ae9dbcb8bc6c8c7f335eacc4f4a8c19170740587
Antoanne Pontes <antoanne@gmail.com>
2016-05-21 10:32:02 -0300

    Discounted cash flow series

---
+++ ifm/R/discounted.csf.R
@@ -0,0 +1,26 @@
+#' @name discounted.csf
+#' @aliases discounted_csf discountedCsf
+#' @param cfs A vector with a series of cash flows.
+#' @param interest.rate A number that represents the nominal Interest Rate, 
+#' presented by year.
+#' @param begin.of.period A boolean that represents if the Tax Rate will be 
+#' applied at the begining of period. FALSE by default, the Tax Rate will be 
+#' applied to the second period.
+#' @export
+#' @return The vector of cash flows incomes/outcomes applying the Tax Rate to
+#' the present time.
+#' @keywords cash flow series
+#' @family financial
+#' @examples
+#'
+#' ex.disc.csf <- discounted.csf(c(-350,100,200,150,75), 0.0619, TRUE)
+#' 
+discounted.csf <- discounted_csf <- discountedCsf <- 
+  function(cfs = c(-350,100,200,150,75), 
+           interest.rate = 0.0619, 
+           begin.of.period = TRUE) 
+  {
+    time.instance <- if(begin.of.period) {0:(length(cfs)-1)} else {1:(length(cfs))}
+    tax.rate <- (1 / ((1 + (interest.rate)) ^ time.instance))
+    return (cfs * tax.rate)
+  }
