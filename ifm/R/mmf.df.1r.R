7873f1ec0edbbdedc9c026ad6329d1506fab159d
Antoanne Pontes <antoanne@gmail.com>
2016-05-21 18:02:22 -0300

    Documenting the function mmf.df.1r #12

--- ifm/R/mmf.df.1r.R
+++ ifm/R/mmf.df.1r.R
@@ -5,7 +5,32 @@
 #' @param mmf.sched A list of schedules
 #' @param mmf.nvp A list of NPV values
 #' @export
-mmf.df.1r <- function(mmf.seq, mmf.sched, mmf.npv, npv.breakeven, npv.selffunding){
+#' @examples 
+#' 
+#' ex.sheet.data <- excel.xls.to.list("resources/spreadsheet.xls")
+#' ex.sheet.data.interest.rate <- ex.sheet.data[[1]]
+#' ex.sheet.data.activities <- ex.sheet.data[[2]]
+#' ex.sheet.data.durations <- ex.sheet.data[[3]]
+#' ex.sheet.data.predecessors <- ex.sheet.data[[4]]
+#' ex.sheet.data.cfs <- ex.sheet.data[[5]]
+#' 
+#' ex.mmf.seq <- mmf.all.sequences(ex.sheet.data.predecessors)
+#' 
+#' ex.mmf <- mmf.npv(ex.sheet.data.cfs,
+#'                   ex.sheet.data.durations,
+#'                   ex.mmf.seq,
+#'                   ex.sheet.data.interest.rate)
+#' 
+#' ex.mmf.shedules <- ex.mmf[['shedules']]
+#' ex.mmf.npv <- ex.mmf[['npv']]
+#' 
+#' ex.mmf.df.1r <- mmf.df.1r(ex.mmf.seq,
+#'                           ex.mmf.schedules,
+#'                           ex.mmf.npv,
+#'                           ex.mmf.npv.selffunding,
+#'                           ex.mmf.npv.breakeven)
+#'                           
+mmf.df.1r <- function(mmf.seq, mmf.sched, mmf.npv, npv.selffunding, npv.breakeven){
   
   all.seq <- list()
   for (seq in mmf.seq) {
